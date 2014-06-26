56437cadc5378f0415ff1747f9400885343e71b7
TehSomeLuigi <olivier.the.olive@gmail.com>
2014-06-26 19:57:42 +0100

    Follow along, LMR ep 5

--- src/main/java/com/someluigi/letsmodfollow/LetsModFollow.java
+++ src/main/java/com/someluigi/letsmodfollow/LetsModFollow.java
@@ -8,13 +8,13 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid= Reference.MOD_ID, name=Reference.MOD_NAME, version="1.7.2-0.0.0")
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = "1.7.2-0.0.0")
 public class LetsModFollow {
     @Mod.Instance(Reference.MOD_ID)
     public static LetsModFollow instance;
 
-	@SidedProxy(clientSide = "com.someluigi.letsmodfollow.proxy.ClientProxy", serverSide = "com.someluigi.letsmodfollow.proxy.ServerProxy")
-	public static IProxy proxy;
+    @SidedProxy(clientSide = "com.someluigi.letsmodfollow.proxy.ClientProxy", serverSide = "com.someluigi.letsmodfollow.proxy.ServerProxy")
+    public static IProxy proxy;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent evt) {
