2497c4da7423a7c584f760e4af8a5fc41693274e
TehSomeLuigi <olivier.the.olive@gmail.com>
2014-06-26 17:53:17 +0100

    Following along, testing IDEA git integration

---
+++ src/main/java/com/someluigi/letsmodfollow/LetsModFollow.java
@@ -0,0 +1,27 @@
+package com.someluigi.letsmodfollow;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="LetsModFollow", name="Lets Mod Follow", version="1.7.2-0.0.0")
+public class LetsModFollow {
+    @Mod.Instance("LetsModFollow")
+    public static LetsModFollow instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent evt) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent evt) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent evt) {
+
+    }
+}
